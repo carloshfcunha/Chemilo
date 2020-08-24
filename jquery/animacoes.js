0f87d232db735c87b7e1014298077d17abe8feb5
Carlos Henrique Furtado da Cunha <carloshfcunha@gmail.com>
2020-08-24 16:43:26 -0300

    Atualização das animações

--- jquery/animacoes.js
+++ jquery/animacoes.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
 
     $('.passos').click( function(){
         if (ii + jj + kk == 3) {
-            $('.passos').not(this).fadeOut(300);
-            $('.passos').not(this).css({width: "0px", height: "0px", padding: "0px", transition: "0.2s"});
-            $(this).css({width: "100%", height: "100%", margin: "0px", cursor: "auto", transition: "0.3s"});
-            $('.passos-titulo').slideUp(300);
-            $('.passos-texto p').slideUp(300);
-            $('#calcular').slideUp(300);
-            $(this).find(".dados").fadeIn(900);
+            $('.passos').not(this).fadeOut(500);
+            $('.passos').not(this).css({width: "0px", height: "0px", padding: "0px", margin: "0px", transition: ".5s"});
+            $(this).css({width: "100%", height: "100%", margin: "0px", cursor: "auto", transition: ".5s"});
+            $('.passos-titulo').slideUp(500);
+            $('.passos-texto p').slideUp(500);
+            $('#calcular').slideUp(500);
+            $(this).find(".dados").slideDown(500);
             ii = 0;
             jj = 0;
         }
@@ -83,7 +83,7 @@ $(document).ready(function(){
     });
 
     $('.fechar').click( function(){
-        $('.passos').fadeIn(300);
+        $('.passos').fadeIn(500);
         if( $(window).width() <= 456){
             $('.passos').css({width: "46%", height: "200px", margin: "2%", padding: "10px", cursor: "pointer"});
         }
@@ -91,10 +91,10 @@ $(document).ready(function(){
             $('.passos').css({width: "200px", height: "183px", margin: "2%", padding: "10px", cursor: "pointer"});
         }
         
-        $('.dados').fadeOut(300);
-        $('.passos-titulo').fadeIn(300);
-        $('.passos-texto p').fadeIn(300);
-        $('#calcular').slideDown(300);
+        $('.dados').slideUp(500);
+        $('.passos-titulo').slideDown(500);
+        $('.passos-texto p').slideDown(500);
+        $('#calcular').slideDown(500);
 
         jj = 1;
     });
